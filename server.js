95ae6ca0bbf4d471a3785a4ce07285e15bca1b5c
ficiki <fikiadsa@gmail.com>
2025-12-16 21:29:47 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from "express";
 import path from "path";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const distPath = path.join(process.cwd(), "dist");
 
 app.use(express.static(distPath));
